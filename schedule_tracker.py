0dbd890199d0b32f94dc75cfe5b94e0cecba3fc2
Alexis Francis <afranc34@students.kennesaw.edu>
2025-04-28 22:39:43 -0400

    Create schedule_tracker.py
    Tracking when users should take medications

---
+++ schedule_tracker.py
@@ -0,0 +1,10 @@
+#This helps users track when medications should be taken and notifthe patient.
+from datetime import datetime, timedelta
+
+def generate_schedule(start_time, interval_hours, doses):
+    schedule = []
+    current_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M")
+    for _ in range(doses):
+        schedule.append(current_time.strftime("%Y-%m-%d %H:%M"))
+        current_time += timedelta(hours=interval_hours)
+    return schedule
